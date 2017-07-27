cf4b01dc29c9e2620ec6815e991e5cf227dfeea0
zhuangchuming <522236495@qq.com>
2017-07-27 13:05:01 +0800

    添加忽略

--- config.py
+++ config.py
@@ -2,8 +2,8 @@
 # -*- coding: UTF-8 -*-
 BaseLoop = 100000#输出json时，循环遍历的基准
 
-serverAddr = 'localhost'
-databaseName = 'tony'
-user = 'chuming'
-password = '123456'
-exportMongo = True
+serverAddr = 'localhost'#服务器地址
+databaseName = 'tony'#数据库名字
+user = 'chuming'#mysql用户
+password = '123456'#mysql密码
+exportMongo = True#是否到处为可以插入mongo的格式
